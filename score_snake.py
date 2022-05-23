32a1bea9d38bb26ec2ece3aa38134e116d6b5237
Sweta Maharjan <60056050+itssss-sweta@users.noreply.github.com>
2022-05-23 20:41:40 +0545

    Update score_snake.py

--- score_snake.py
+++ score_snake.py
@@ -6,8 +6,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
-        
-        self.highscore=0
+       with open("file.txt") as data:
+            self.highscore=int(data.read())
         self.penup()
         self.goto(x=0,y=270)
         self.color("white")
@@ -23,6 +23,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore=self.score
+            with open("file.txt",mode="w") as data:
+                data.write(f"{self.highscore}")
             
         self.score=0
         self.update_score()
